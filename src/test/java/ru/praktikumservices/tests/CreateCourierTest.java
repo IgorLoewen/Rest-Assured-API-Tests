d17f3b2b9f5ac0cdf81d289776024d80b18ca95a
TT <iloewen79@gmx.de>
2024-12-08 18:52:49 +0100

    Добавил тест на не валидное создание курьера второй раз с теми же даннымы

--- src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
+++ src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CreateCourierTest extends TestsSetUp {
 
-    @Test
+   /* @Test
     public void courierCanBeCreated() {
         // Подготовка тела запроса
         String requestBody = "{ \"login\": \"ыалолто\", \"password\": \"2234\", \"firstName\": \"Test\" }";
@@ -26,5 +26,26 @@ public class CreateCourierTest extends TestsSetUp {
                 .statusCode(201) // Проверяем код ответа
                 .and()
                 .body("ok", equalTo(true)); // Проверяем тело ответа
+    }*/
+
+    @Test
+    public void courierCanNotBeCreatedTwice() {
+        // Подготовка тела запроса
+        String requestBody = "{ \"login\": \"ыалолто\", \"password\": \"2234\", \"firstName\": \"Test\" }";
+
+        // Отправка POST-запроса
+        Response response = given()
+                .header("Content-type", "application/json") // Указываем заголовок
+                .and()
+                .body(requestBody) // Тело запроса
+                .when()
+                .post("/api/v1/courier"); // Эндпоинт для создания курьера
+
+        // Проверка результата
+        response.then()
+                .statusCode(409) // Проверяем код ответа
+                .and()
+                .body("message", equalTo("Этот логин уже используется. Попробуйте другой.")); // Проверяем тело ответа
+                                          //баг в документации ЯП там ожидается "Этот логин уже используется"
     }
 }

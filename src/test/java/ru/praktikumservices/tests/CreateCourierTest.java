ee26a4c02974ddb05af5546d22cf961c6d360fee
TT <iloewen79@gmx.de>
2024-12-08 17:28:17 +0100

    Добавлена первая проверка courierCanBeCreated

---
+++ src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
@@ -0,0 +1,30 @@
+package ru.praktikumservices.tests;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CreateCourierTest extends TestsSetUp {
+
+    @Test
+    public void courierCanBeCreated() {
+        // Подготовка тела запроса
+        String requestBody = "{ \"login\": \"ыалолто\", \"password\": \"2234\", \"firstName\": \"Test\" }";
+
+        // Отправка POST-запроса
+        Response response = given()
+                .header("Content-type", "application/json") // Указываем заголовок
+                .and()
+                .body(requestBody) // Тело запроса
+                .when()
+                .post("/api/v1/courier"); // Эндпоинт для создания курьера
+
+        // Проверка результата
+        response.then()
+                .statusCode(201) // Проверяем код ответа
+                .and()
+                .body("ok", equalTo(true)); // Проверяем тело ответа
+    }
+}

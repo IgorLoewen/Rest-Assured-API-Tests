e6bec8f6678f6215e5981a9d94414c0249c547b8
TT <iloewen79@gmx.de>
2024-12-10 06:47:21 +0100

    Добавил тест - нельзя создать двух одинаковых курьеров;

--- src/main/java/ru/praktikumservices/steps/CourierSteps.java
+++ src/main/java/ru/praktikumservices/steps/CourierSteps.java
@@ -10,17 +10,6 @@ import static ru.praktikumservices.data.Data.*;
 
 public class CourierSteps {
 
-    @Step("Логин курьера с дефолтными данными")
-    public Response loginCourier() {
-        String loginRequestBody = "{ \"login\": \"" + login + "\", \"password\": \"" + password + "\" }";
-
-        return given()
-                .header("Content-type", "application/json")
-                .body(loginRequestBody)
-                .when()
-                .post("/api/v1/courier/login");
-    }
-
     @Step("Создание курьера с дефолтными данными")
     public Response createCourier() {
         String createRequestBody = "{ \"login\": \"" + login + "\", \"password\": \"" + password + "\", \"firstName\": \"" + firstName + "\" }";
@@ -32,7 +21,28 @@ public class CourierSteps {
                 .post("/api/v1/courier");
     }
 
-    @Step("Удаление курьера с ID: {courierId}")
+    @Step("Логин курьера с дефолтными данными")
+    public Response loginCourier() {
+        String loginRequestBody = "{ \"login\": \"" + login + "\", \"password\": \"" + password + "\" }";
+
+        return given()
+                .header("Content-type", "application/json")
+                .body(loginRequestBody)
+                .when()
+                .post("/api/v1/courier/login");
+    }
+
+
+
+    @Step("Извлечение id: курьера из ответа логина")
+    public Integer extractCourierId(Response loginResponse) {
+        return loginResponse.then()
+                .statusCode(200)
+                .extract()
+                .path("id");
+    }
+
+    @Step("Удаление курьера с id: {courierId}")
     public void deleteCourier(Integer courierId) {
         given()
                 .header("Content-type", "application/json")
@@ -41,14 +51,6 @@ public class CourierSteps {
                 .then()
                 .statusCode(200)
                 .body("ok", equalTo(true));
-    }
-
-    @Step("Извлечение ID курьера из ответа логина")
-    public Integer extractCourierId(Response loginResponse) {
-        return loginResponse.then()
-                .statusCode(200)
-                .extract()
-                .path("id");
     }
 }
 
--- src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
+++ src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
@@ -1,58 +1,42 @@
 package ru.praktikumservices.tests;
 
-import static ru.praktikumservices.data.Data.*;
-
 import io.restassured.response.Response;
 import org.junit.Test;
 import ru.praktikumservices.steps.CourierSteps;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 
 public class CreateCourierTest extends TestsSetUp {
 
     private final CourierSteps courierSteps = new CourierSteps();
+    private Integer courierId;
 
 
-    @Test
+
+    @Test //  Курьера можно создать;
     public void courierCanBeCreatedLoggedAndDeleted() {
-        // Создание курьера
         Response createResponse = courierSteps.createCourier();
         createResponse.then().statusCode(201).body("ok", equalTo(true));
-
-        // Логин курьера
         Response loginResponse = courierSteps.loginCourier();
-
-        // Получение ID курьера
         Integer courierId = courierSteps.extractCourierId(loginResponse);
-
-        // Удаление курьера
         courierSteps.deleteCourier(courierId);
     }
+
+    @Test //
+    public void courierCannotBeCreatedTwice() {
+        Response firstCreateResponse = courierSteps.createCourier();
+        firstCreateResponse.then().statusCode(201).body("ok", equalTo(true));
+        Response secondCreateResponse = courierSteps.createCourier();
+        secondCreateResponse.then().statusCode(409).body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
+        Response loginResponse = courierSteps.loginCourier();
+        Integer courierId = courierSteps.extractCourierId(loginResponse);
+        courierSteps.deleteCourier(courierId);
+    }
+
 
 
 }
 
 
 
-   /*  @Test
-    public void courierCanNotBeCreatedTwice() {
-        // Подготовка тела запроса
-        String requestBody = "{ \"login\": \"ыалолто\", \"password\": \"2234\", \"firstName\": \"Test\" }";
-
-        // Отправка POST-запроса
-        Response response = given()
-                .header("Content-type", "application/json") // Указываем заголовок
-                .and()
-                .body(requestBody) // Тело запроса
-                .when()
-                .post("/api/v1/courier"); // Эндпоинт для создания курьера
-
-        // Проверка результата
-        response.then()
-                .statusCode(409) // Проверяем код ответа
-                .and()
-                .body("message", equalTo("Этот логин уже используется. Попробуйте другой.")); // Проверяем тело ответа
-                                          //баг в документации ЯП там ожидается "Этот логин уже используется"
-    }*/

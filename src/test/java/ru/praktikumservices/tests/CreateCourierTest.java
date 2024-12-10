3ef4407187e7196b4d97d29a00c3ea3fe8c59929
TT <iloewen79@gmx.de>
2024-12-10 14:14:49 +0100

    Все тесты на создание и логин курьера готовы

--- src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
+++ src/test/java/ru/praktikumservices/tests/CreateCourierTest.java
@@ -12,6 +12,10 @@ public class CreateCourierTest extends TestsSetUp {
     private final CourierSteps courierSteps = new CourierSteps();
 
     @Test // Курьера можно создать
+          // запрос возвращает правильный код ответа
+          // успешный запрос возвращает ok: true
+          // курьер может авторизоваться;
+          // успешный запрос возвращает id.
     public void courierCanBeCreatedLoggedAndDeleted() {
         Response createResponse = courierSteps.createCourier(VALID_COURIER_REQUEST_BODY);
         createResponse.then().statusCode(201).body("ok", equalTo(true));
@@ -21,6 +25,9 @@ public class CreateCourierTest extends TestsSetUp {
     }
 
     @Test // Нельзя создать двух одинаковых курьеров
+          // если создать пользователя с логином, который уже есть, возвращается ошибка.
+          // курьер может авторизоваться;
+          // успешный запрос возвращает id.
     public void courierCannotBeCreatedTwice() {
         Response firstCreateResponse = courierSteps.createCourier(VALID_COURIER_REQUEST_BODY);
         firstCreateResponse.then().statusCode(201).body("ok", equalTo(true));
@@ -32,10 +39,24 @@ public class CreateCourierTest extends TestsSetUp {
     }
 
     @Test // Нельзя создать курьера без обязательных полей
+          // для авторизации нужно передать все обязательные поля;
+          // чтобы создать курьера, нужно передать в ручку все обязательные поля;
+          // если одного из полей нет, запрос возвращает ошибку;
     public void courierCannotBeCreatedWithoutRequiredFields() {
         for (String body : INVALID_COURIER_REQUEST_BODIES) {
             Response response = courierSteps.createCourier(body);
             response.then().statusCode(400).body("message", equalTo("Недостаточно данных для создания учетной записи"));
         }
     }
+
+    @Test // если авторизоваться под несуществующим пользователем, запрос возвращает ошибку;
+    public void loginUnavailableRegister(){
+    Response loginUnavailableRegister = courierSteps.loginCourier(LOGIN_REQUEST_BODY);
+    loginUnavailableRegister.then().statusCode(404).body("message", equalTo("Учетная запись не найдена"));
+    }
+
+
+
+
+
 }
